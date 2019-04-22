3d033d3a654f673af8fadaa5563e8a01c2a79ebf
ruekurniawan <rue.kurniawan@github.com>
2019-04-22 20:45:45 +0700

    pertanyaan yang baru ada

---
+++ src/api/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+import 'firebase/firestore'
+
+let config = {
+    apiKey: "AIzaSyBMHH8bPe7-A69cyI-QrBQFTuJBMOl_xhU",
+    authDomain: "howwellyouknowus.firebaseapp.com",
+    databaseURL: "https://howwellyouknowus.firebaseio.com",
+    projectId: "howwellyouknowus",
+    storageBucket: "howwellyouknowus.appspot.com",
+    messagingSenderId: "493616838925"
+};
+  
+let db = firebase.initializeApp(config);
+
+export default db
