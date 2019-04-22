df5236e4d950bb6c9a74c8c27b7ef2262648a4ef
William Suryawan <suryawan.william@gmail.com>
2019-04-22 16:39:02 +0700

    update cdn and api folder

---
+++ src/api/firebase.js
@@ -0,0 +1,18 @@
+// import 'firebase/firestore';
+
+const firebase = require('firebase/app');
+require('firebase/firestore');
+require('dotenv').config()
+
+const config = {
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+};
+
+firebase.initializeApp(config);
+const db = firebase.firestore()
+export default db
